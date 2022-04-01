b121799a569096288cacb2597dd0673370513be9
kinhopoa <marcosfraga05@gmail.com>
2022-03-31 22:54:47 -0300

    terceira att, paginas de produtos

--- controllers/ProdutoController.js
+++ controllers/ProdutoController.js
@@ -4,6 +4,24 @@ const ProdutoController = {
     },
     meusPedidos: (req, res) => {
         res.render('meuspedidos')
+    },
+    canecas: (req, res) => {
+        res.render('canecas')
+    },
+    camisetas: (req, res) => {
+        res.render('camisetas')
+    },
+    chapeus: (req, res) => {
+        res.render('chapeus')
+    },
+    games: (req, res) => {
+        res.render('games')
+    },
+    moletons: (req, res) => {
+        res.render('moletons')
+    },
+    vestuario: (req, res) => {
+        res.render('vestuario')
     }
 }
 
